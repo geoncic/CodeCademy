54a04f383ec64d0f8ae117379d57f924c754c32a
geoncic <geoncic@gmail.com>
2022-04-24 13:14:06 -0500

    // WIP - Life Expectancy and GDP Data Analysis

--- Projects/Life-Expectancy-and-GDP-Starter/main.py
+++ Projects/Life-Expectancy-and-GDP-Starter/main.py
@@ -1,7 +1,12 @@
 import argparse
 import os.path
-import csv
-from collections import defaultdict
+# import csv
+# from collections import defaultdict
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+
 
 
 INPUT_TXT = os.path.join(os.path.dirname(__file__), 'all_data.csv')
@@ -12,19 +17,35 @@ def read_file():
     parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
     args = parser.parse_args()
 
-    with open(args.data_file) as f:
-        file = csv.DictReader(f)
-        data = defaultdict(list)
+    print(args.data_file)
 
-        for row in file:
-            print(row)
+    re = pd.read_csv(args.data_file)
 
-        return data
+    return re
+
+
+    # with open(args.data_file) as f:
+    #     file = csv.DictReader(f)
+    #     data = defaultdict(list)
+    #
+    #     for row in file:
+    #         print(row)
+    #
+    #     return data
 
 
 def main():
-    data = read_file()
-    print(data)
+    all_data = read_file()
+    print(all_data.head())
+    print(all_data.columns)
+    print(all_data.info())
+    print(all_data['Country'].unique())
+
+    # plt.subplots()
+    # plt.show()
+
+    plt.show(block=True)
+    plt.interactive(False)
 
 
 if __name__ == "__main__":
