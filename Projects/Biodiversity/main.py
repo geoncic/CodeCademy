a9fa4760388e00483fca79221d59a02ede3b05ec
Geo <geoncic@gmail.com>
2022-04-27 10:50:21 -0500

    // Data cleaning - Biodiversity

--- Projects/Biodiversity/main.py
+++ Projects/Biodiversity/main.py
@@ -4,6 +4,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# set to display dataframe rows without truncating
+pd.set_option('display.max_rows', 40)
+pd.set_option('display.max_columns', None)
+
+# set fontsize for all plots
+plt.rcParams['font.size'] = 14
+
 
 def read_file(filename):
     input_txt = os.path.join(os.path.dirname(__file__), filename)
@@ -19,26 +26,91 @@ def read_file(filename):
 
 
 def main():
+
+    # Import data
     observations = read_file('observations.csv')
     species_info = read_file('species_info.csv')
-    print(observations.info())
-    print(species_info.info())
-    print(observations.head())
-    print(species_info.head())
+
+    # check the construction of the observation data
+    # print(observations.info())
+    # print(observations.head())
+    #
+    # print(species_info.info())
+    # print(species_info.head())
+
+    # check for duplicate records
+    dup = observations[observations.duplicated(keep = False)]
+    dup = dup.sort_values(['scientific_name'])
+
+    # print(observations.shape)
+    observations = observations.drop_duplicates()
+    # print(observations.shape)
+
+    # check for records which have the same scientific_name and park_name (possibly conflicting observation counts)
+
+    dup2 = observations[observations.duplicated(subset=['scientific_name', 'park_name'], keep = False)]
+    dup2 = dup2.sort_values(['scientific_name', 'park_name'])
+    # print(dup2)
+
+    # check shape of duplicate dataset
+    # print(dup2.shape)
+
+    # drop duplicates by using the mean of the observations to represent the approximate number of observations for duplicate records
+
+    # print(observations.groupby(['scientific_name', 'park_name'], as_index = False).mean())
+
+    observations_cleaned = observations.groupby(['scientific_name', 'park_name'], as_index = False).mean()
+    observations_cleaned = observations_cleaned.sort_values(['scientific_name', 'park_name'])
+
+    # check for records which have the same category and scientific_name
+    dup3 = species_info[species_info.duplicated(subset =['category', 'scientific_name'], keep = False)]
+    dup3 = dup3.sort_values(['category', 'scientific_name'])
+
+
+    # drop duplicates and keep the first record
+    species_info_cleaned = species_info.drop_duplicates(subset=['category', 'scientific_name'])
+
+    # Merge Dataframes
+
+    df = observations_cleaned.merge(species_info_cleaned, how = 'left')
+    print(df.head())
+
+    # check for N/A values
+    print(df.isna().sum())
+
+    # get an overview of the quantitative column data
+    print(df['observations'].describe())
+
+    # select only categorical columns
+    cat_columns = df.select_dtypes(include='object')
+
+    # get an overview of the categorical columns
+
+    print(cat_columns.describe())
 
     species_info.fillna('No Intervention', inplace=True)
     cons_category = species_info[species_info['conservation_status'] != "No Intervention"].groupby("conservation_status").size()
 
-    print(cons_category)
-    print(cons_category.index)
 
-    fig1, ax1 = plt.subplots()
-    fig2, ax2 = plt.subplots()
+
+    ## ToDo: Define goals and analyze the data
+
+
+
+
+
+    # print(cons_category)
+    # print(cons_category.index)
+
+    # fig1, ax1 = plt.subplots()
+    # fig2, ax2 = plt.subplots()
+
+    # print(species_info.columns)
 
     # ax1 = plt.pie(cons_category, labels = cons_category.index, colors=sns.color_palette('pastel'), autopct='%.0f%%')
     # plt.show()
     # plt.clf()
-    ax2 = sns.countplot(data = species_info, x = 'conservation_status', ax = ax2)
+    # ax2 = sns.countplot(data = species_info, x = 'conservation_status', ax = ax2)
 
     plt.show()
     plt.interactive(False)
